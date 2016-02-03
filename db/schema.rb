c7271d08ac2a9d3f660fc2d8d3985dd81a26fc42
Buddy Magsipoc <keikun17@gmail.com>
2016-02-03 15:29:05 +0800

    User games。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201132600) do
+ActiveRecord::Schema.define(version: 20160203071159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20160201132600) do
     t.string   "icon_small"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "games_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "game_id"
+    t.index ["game_id", "user_id"], name: "index_games_users_on_game_id_and_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
