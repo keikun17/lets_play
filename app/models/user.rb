fabf9bec150d7d0a086d7ea5f3ec388dbfdd5ef8
Buddy Magsipoc <keikun17@gmail.com>
2016-02-12 00:12:59 +0800

    Remove unused。

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,15 +18,6 @@ class User < ApplicationRecord
 
   attr_accessor :login
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_hash).first
-    end
-  end
-
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
