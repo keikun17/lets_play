67b948ddd83617ee45bb230a9bbfc3a4182d3ac3
Buddy Magsipoc <keikun17@gmail.com>
2016-02-11 23:56:55 +0800

    Log in with username。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,36 @@ class User < ApplicationRecord
 
   has_many :invitations, foreign_key: :sender_id
   has_many :invites,  class_name: "Invitation", foreign_key: :receiver_id
+
+  #------------------------------------------------------------
+  # Allow users to log in with username
+  #------------------------------------------------------------
+  validates :username,
+    uniqueness: { case_sensitive: false },
+    format:     { with: /^[a-zA-Z0-9_\.]*$/, :multiline => true }
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
 end
